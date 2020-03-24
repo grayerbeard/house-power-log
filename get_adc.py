d22563820c944ea7f285c11010d03d8eb700471f
david <djgtorrens@gmail.com>
2020-03-24 18:27:54 +0000

    so far so March 24th

--- get_adc.py
+++ get_adc.py
@@ -1,46 +1,95 @@
 import Adafruit_ADS1x15
 import time
+from datetime import datetime
+from math import sqrt
 
-class class_get_ads(object):
+class class_get_adc(object):
 	# Rotating Buffer Class
 	# Initiate with just the size required Parameter
 	# Get data with just a position in buffer Parameter
-	def __init__(self,scan_size,diff_channel,config,default_gain,top_limit,bottom_limit):
+	def __init__(self,scan_size,diff_channel,config,default_gain,top_limit,bottom_limit,input_offset_mv,input_amp_gain,CT_resister,CT_ratio):
 		#initialization
 		self.__config = config
 		self.gain = default_gain
 		self.__adc=Adafruit_ADS1x15.ADS1015()
-		self.last_scan_size =  max_scan_size
+		self.scan_size =  scan_size
 		self.__diff_channel = diff_channel
-		self.reading = [0.0]*(max_scan_size + 1)
-		self.scan_interval =  [datetime.now()]*(scan_size + 1)
+		self.reading = [0.0]*(self.scan_size + 1)
+		self.adc_milli_volts = [0.0]*(self.scan_size + 1)
+		self.input_milli_volts = [0.0]*(self.scan_size + 1)
+		self.peak_current = [0.0]*(self.scan_size + 1)
+		self.rms_current = [0.0]*(self.scan_size + 1)
+		self.reading_time = [0.0]*(self.scan_size + 1)
+		self.reading_gain = [default_gain]*(self.scan_size + 1)
+		self.scan_interval =  [0.1]*(self.scan_size + 1)
 		self.gain_change_count = 0
-		self__top_limit = top_limit
-		self__bottom_limit = bottom_limit              
+		self.__top_limit = top_limit
+		self.__bottom_limit = bottom_limit 
+		self.__input_offset_mv = input_offset_mv
+		self.__input_amp_gain = input_amp_gain
+		self.__CT_resister = CT_resister
+		self.__CT_ratio = CT_ratio
+            
 
-	def do_scan(self):
+	def do_scan(self,do_resets,change_limit,debug_flag):
 		self.gain_change_count = 0
 		scan_count = 0
+		time_from_gain_change = 0.0
 		reading_ac_max = 0
 		last_time = datetime.now()
-		while scan_count < scan_size:
+		rms_current_total = 0.001
+		rms_totaller_count = 0
+		rms_totaler_enable = False
+		rms_totaller_cycle_count = -1
+		while scan_count < self.scan_size:
+			#print(rms_totaler_enable)
+			self.reading_gain[scan_count] = self.gain
 			self.reading[scan_count] = self.__adc.read_adc_difference(self.__diff_channel,gain = self.gain)
+			if abs(self.reading[scan_count]) > reading_ac_max:
+				reading_ac_max = abs(self.reading[scan_count])
+			self.reading_time[scan_count] = self.reading_time[scan_count-1] + self.scan_interval[scan_count]
+			self.adc_milli_volts[scan_count] = (self.reading[scan_count] * 2/self.reading_gain[scan_count]) - self.__input_offset_mv
+			self.input_milli_volts[scan_count] = self.adc_milli_volts[scan_count] / self.__input_amp_gain
+			self.peak_current[scan_count] = self.input_milli_volts[scan_count]/self.__CT_resister/self.__CT_ratio
+			#Check for just been a zero crossing negative to positive
+			if rms_totaler_enable:
+				rms_current_total = rms_current_total + (self.peak_current[scan_count] * self.peak_current[scan_count])
+			#print(round((self.peak_current[scan_count] * self.peak_current[scan_count]),2),round(self.peak_current[scan_count],2),rms_current_total)
+			if (self.peak_current[scan_count] * self.peak_current[scan_count-1] < 0) and (self.peak_current[scan_count] > 0) : 
+				rms_totaler_enable = True
+				rms_totaller_cycle_count +=1
+				self.rms_current[scan_count] = 2 * sqrt(rms_current_total/(scan_count+1))
+			elif scan_count == 0:
+				self.rms_current[scan_count] = 0
+			else:
+				self.rms_current[scan_count] = self.rms_current[scan_count-1]
 			this_time = datetime.now()
-			scan_interval = 1000*(this_time - last_time ).total_seconds()
+			self.scan_interval[scan_count] = 1000*((this_time - last_time ).total_seconds())
+			time_from_gain_change = time_from_gain_change + self.scan_interval[scan_count]
 			last_time = this_time
-			if abs(reading[scan_count]) > self__top_limit:
-				if (self.gain > 1) and gain_change_count < 4:
-					self.gain = int(self.gain/2)
-					reading_ac_max = 0
-					scan_count = 0
-			else:
-				if abs(reading[scan_count] > reading_ac_max:
-					reading_ac_max = abs(reading[scan_count]
-			if scan_count > int(scan_size/4):
-				if (reading_ac_max < self__bottom_limit) and gain_change_count < 4:
-					if self.gain < 16:
-						self.gain = int(self.gain*2)
-						scan_count = 0
-			else:
-				scan_count += 1
+			if debug_flag : print("#:%3d,Time:%6.2f,Gain:%2d,adcRdg:%5d,adcIn_mv:%5.2f, \
+mvIn:%5.2f,Ipk%5.2f,rms:%5.2f" %
+(scan_count,self.reading_time[scan_count],self.gain,self.reading[scan_count],self.adc_milli_volts[scan_count],
+self.input_milli_volts[scan_count],self.peak_current[scan_count],self.rms_current[scan_count]))
+			if abs(self.reading[scan_count]) > self.__top_limit and (self.gain > 1) and self.gain_change_count < change_limit:
+				self.gain = int(self.gain/2)
+				self.gain_change_count += 1
+				reading_ac_max = 0
+				time_from_gain_change = 0
+				if do_resets: 
+					scan_count = -1
+					rms_total = 0.001
+					rms_totaler_enable = False
+					self.rms_current[scan_count] = 0
+			elif time_from_gain_change > 60 and (reading_ac_max < self.__bottom_limit) and (self.gain_change_count < change_limit) and (self.gain < 16):
+				self.gain = int(self.gain*2)
+				self.gain_change_count += 1
+				reading_ac_max = 0
+				time_from_gain_change = 0
+				if do_resets: 
+					scan_count = -1
+					rms_total = 0.001
+					rms_totaler_enable = False
+					self.rms_current[scan_count] = 0
+			scan_count += 1
 		return self.gain_change_count
--- house-power.py
+++ house-power.py
@@ -21,7 +21,7 @@
 #   - 3 = Channel 2 minus channel 3    amp /2 gain xx
 
 import time
-from math import sqrt
+#from math import sqrt
 
 
 #from ads_lib import ADS1015
@@ -36,112 +36,62 @@ from datetime import datetime
 from utility import pr,make_time_text,send_by_ftp
 from text_buffer import class_text_buffer
 from config import class_config
-from get_adc import class_get_ads
+from get_adc import class_get_adc
 
-scan_size = 200
+
+scan_size = 100
 diff_channel = 3
+
+default_gain = 1
 top_limit = 2020
 bottom_limit = 980
-config = class_config()
-adc = class_get_ads(scan_size,diff_channel,config,top_limit,bottom_limit)
+#input_offset_mv = -6.5  # tested for channel 2
+#input_amp_gain = 100 # tested for channel 2
+input_offset_mv = 19 # tested for channel 3
+input_amp_gain = 10.615 # tested for channel 3
+CT_ratio = 1 # mAmps out to Amps in.
+CT_resister = 22
 
-headings = ["Time","Time Step","Gain","Reading"] #"Rdg13","Rdg23","ac03","ac13","AC23"] # "currents RMS","Current","ZeroCrosses","Spare1","Spare2"]
+
+config = class_config()
+adc = class_get_adc(scan_size,diff_channel,config,default_gain,top_limit,bottom_limit,input_offset_mv,input_amp_gain,CT_resister,CT_ratio)
+
+headings = ["Time","Time Step","gain","Reading","adc Milli Volts","Input mv","I","Irms" ] 
 analog_buffer = class_text_buffer(headings,config)
 
-#reading_0_3 = [0.0]*(scan_size + 1)
-#reading_1_3 = [0.0]*(scan_size + 1)
-#reading_2_3 = [0.0]*(scan_size + 1)
-#gain = [0.0]*(scan_size + 1)
-#reading_ac_0_3 = [0.0]*(scan_size + 1)
-#reading_ac_1_3 = [0.0]*(scan_size + 1)
-
-#reading_ac_2_3 = [0.0]*(scan_size + 1)
-#reading_square_total = [0.0]*(scan_size + 1)
-#reading_ac_max = [0.0]*(scan_size + 1)
-#current_rms = [0.0]*(scan_size + 1)
-#current = [0.0]*(scan_size + 1)
-#reading_time = [0.0]*(scan_size + 1)
-#time_step = [0.0]*(scan_size + 1)
-#GAIN = 1
-#last_cycle_max = [0.0]*(scan_size + 1)
-#zero_crossing = [0]*(scan_size + 1)
-
-
-#reference = explorerhat.analog.one.read()
-#reference = adc.read_adc(0, gain=GAIN)
-
-#rc = 1
-#print(reference)
-
-not_finished = True
-
 start_time = datetime.now()
-#reading_0_3[0] = adc.read_adc_difference(1,gain = GAIN)
-#reading_1_3[0] = adc.read_adc_difference(2,gain = GAIN)
-#reading_2_3[0] = adc.read_adc_difference(3,gain = GAIN)#Read the difference between two ADC channels and return the ADC value
-#as a signed integer result.  Differential must be one of:
-#   - 0 = Channel 0 minus channel 1    not used this project 
-#   - 1 = Channel 0 minus channel 3    original buffered   
-#   - 2 = Channel 1 minus channel 3    amp 1 gain xx   
-#   - 3 = Channel 2 minus channel 3    amp /2 gain xx
-reading_time[0] = 1000*(datetime.now() - start_time).total_seconds()
+do_resets = True
+change_limit = 4
+debug_flag = True
+print("Change Count: ",adc.do_scan(do_resets,change_limit,debug_flag))
+read_time = 1000*(datetime.now() - start_time).total_seconds()
 
-GAIN = int(1)
+adc.reading_time[0] = 0
 
-gain_scan = 0
-gain_scan_limit = int(scan_size/5)
-
-cal0 = 1 # 2
-cal1 = 1 #-0.593
-cal2 = 1 # 0.172
-offset0 = 0 #-0.625
-offset1 = 0 # 3.375
-offset2 = 0 # -12 
+for rc in range(1,adc.scan_size,1):
 
 
 
-#print( reference)
-adc.read_adc_difference
-
-read_time = (end_time - start_time).total_seconds()
-
-for rc in range(0,scan_size+1,1):
-	#if rc >=1:
-		#reading_ac[rc] = abs(reading[rc])
-		#if reading_ac[rc]>reading_ac_max["Date_time","Time Step","gain","Rdg01","Rdg23","ac01","ac23,"currents RMS","Current","ZeroCrosses","Spare1","Spare2"rc-1]:
-		#	reading_ac_max[rc] = abs(reading[rc])
-		#else:
-		#	reading_ac_max[rc] = reading_ac_max[rc-1]
-		#if reading_ac[rc]>reading_ac_0_1_max[rc-1]:
-		#	reading_ac_max[rc] = abs(reading[rc])
-		#else:
-		#	reading_ac_max[rc] = reading_ac_max[rc-1]
-		#time_step[rc] = 1000*(reading_time[rc]- reading_time[rc-1]).total_seconds()
-		#reading_square_total[rc] = reading_square_total[rc-1] + (reading_ac[rc] * reading_ac[rc])
-		#current_rms[rc] =  20.826*sqrt(reading_square_total[rc]/rc)
-		#current[rc] = 14.598 * reading_ac_max[rc]
-
-#                0           1          2       3       4       5       6     7      8             9            10      11
-#headings = ["Date_time","Time Step","gain","Rdg003","Rdg13","Rdg23","ac03","ac13","AC23"]       currents RMS","Current"       ,"ZeroCrosses","Spare1","Spare2"]
-	analog_buffer.line_values[0] = str(reading_time[rc])
-	analog_buffer.line_values[1] = str(reading_time[rc] - reading_time[rc-1])
-	analog_buffer.line_values[2] = str(gain[rc])
-	analog_buffer.line_values[3] = str(reading_0_3[rc])
-	analog_buffer.line_values[4] = str(reading_1_3[rc])  
-	analog_buffer.line_values[5] = str(reading_2_3[rc])  
-	analog_buffer.line_values[6] = str(reading_ac_0_3[rc])
-	analog_buffer.line_values[7] = str(reading_ac_1_3[rc])
-	analog_buffer.line_values[8] = str(reading_ac_2_3[rc])
+#                0        1        2       3          4                5       6    7    8             9            10      11
+#headings = ["Time","Time Step","gain","Reading","adc Milli Volts","Input mv","I","Irms"    
+	analog_buffer.line_values[0] = str(adc.reading_time[rc])
+	analog_buffer.line_values[1] = str(adc.scan_interval[rc])
+	analog_buffer.line_values[2] = str(adc.reading_gain[rc])
+	analog_buffer.line_values[3] = str(adc.reading[rc])
+	analog_buffer.line_values[4] = str(adc.adc_milli_volts[rc])  
+	analog_buffer.line_values[5] = str(round(adc.input_milli_volts[rc],3))  
+	analog_buffer.line_values[6] = str(round(adc.peak_current[rc],3))
+	analog_buffer.line_values[7] = str(round(adc.rms_current[rc],3))
 #	analog_buffer.line_values[7] = str(current_rms[rc])
 #	analog_buffer.line_values[8] = str(current[rc])
 #	analog_buffer.line_values[9] = str(zero_crossing[rc])
 #	analog_buffer.line_values[10] = "Spare1 "
 #	analog_buffer.line_values[11] = "Spare2 "
-	analog_buffer.just_log(True,0,reading_time[rc],1234)
+	analog_buffer.just_log(True,0,adc.reading_time[rc],1234)
 
 end_file_time = datetime.now()
 
-file_time = (end_file_time - end_time).total_seconds()
+file_time = 1000*(datetime.now() - start_time).total_seconds() - read_time
 
 print("Read Time",read_time)
 print("File Time",file_time)
